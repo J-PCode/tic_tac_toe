138f364aff50ac2e0e76a3a2b715f5b3d22d3cbc
J-PCode <89179060+J-PCode@users.noreply.github.com>
2022-08-10 06:19:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 // game module, check results, ends game when someone won it and resets after request
-// User interface, show UI tasks: show info for the game, results and gameboard.
+
+const cells = document.querySelectorAll('.cell');
+const gameboard = (() => {
+
+	//generate board array
+	const board = [
+	"","","",
+	"","","",
+	"","",""];
+	
+  const setBoard = () => {
+    for (let i = 0; i < cells.length; < 8; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+    
+}
+const createPlayer = (name, marker) => {
+	return {name, marker};
+	}
+	
+/* add event listeners
+Document.querySelector('#array-id').addEventListener('click', (e) => ja mitä sitten tehdään?
+Lisätään merkki siihen mitä pelaaja painoi, lisätään tulos taulukkoon
+e.textContent = activeplayer.marker;
+
+ 
+
+Tarkistetaan pelin tila ja vaihdetaan mahdollisesti vuoro
+// User interface, show UI tasks: show info for the game, results and gameboard.*/
