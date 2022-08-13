b87c0f352fd6089f82cf5a9b954b817f5117df24
J-PCode <orjala.juho@gmail.com>
2022-08-13 08:22:42 +0300

    some copy paste code to start with

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 // game module, check results, ends game when someone won it and resets after request
 
-const cells = document.querySelectorAll('.cell');
-const gameboard = (() => {
+// player factory create player and playturn
+const playerFactory = (name, mark) => {
+	const playTurn = (board, cell) => {
+		const idx = board.cells.findIndex(position => position === cell);
+		if (board.boardArray[idx] === ''){
+			board.render();
+			return idx;
+		}
+		return null;
+	};
+	return {name, mark, playTurn};
+	};
 
-	//generate board array
-	const board = [
-	"","","",
-	"","","",
-	"","",""];
-	
-  const setBoard = () => {
-    for (let i = 0; i < cells.length; < 8; i++) {
-		cells[i].innerText = '';
-		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick, false);
-	}
-}
-    
-}
-const createPlayer = (name, marker) => {
-	return {name, marker};
-	}
+	const boardModule = (() => {
+		let boardArray = ['', '', '', '', '', '', '', '', ''];
+		const gameboard = document.querySelector('#board');
+		const cells = Array.from(document.querySelectorAll('.cell'));
+		let winner = null;
+
+		const render = () => {
+			boardArray.forEach((mark, idx) => {
+				cells[idx].textContent = boardArray[idx];
+			});
+		};
+		const reset = () => {
+			boardArray = ['', '', '', '', '', '', '', '', ''];
+		};
+
+		const checkWin = () => {
+			const winArrays = [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+				[0, 4, 8],
+				[2, 4, 6],
+			];
+
+			winArrays.forEach((combo) => {
+				if (boardArray[combo[0]] && boardArray[combo[0]] === boardArray[combo[1]] && boardArray[combo[0]] === boardArray[combo[1]]) {
+					winner = 'current';
+				}
+			});
+			return winner || (boardArray.includes('') ? null : 'tie');
+		};
+
+		return {
+			render, gameboard, cells, boardArray, checkWin, reset,
+		};
+	})();
 	
 /* add event listeners
 Document.querySelector('#array-id').addEventListener('click', (e) => ja mitä sitten tehdään?
